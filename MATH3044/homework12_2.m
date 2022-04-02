576254c46a5594e8c6a0857be354558cf30a2027
jackie gan <tuj90887@temple.edu>
2022-04-02 14:08:33 -0400

    update

--- MATH3044/homework12_2.m
+++ MATH3044/homework12_2.m
@@ -1,4 +1,13 @@
 %Jackie Gan
 %MATH 3044
 %Homework12_2, Parabolic Partial Differential Equations
+%Q2, 5a
+format long
 
+close all
+clear
+clc
+
+%Backward difference methods
+m=3; T=0.1; N=2;
+Freal = @(x,t)2*exp(-1*(pi^2/4)*t)*sin(2*pi*x);
