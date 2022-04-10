50bbccadfedff01c130837e7e26d83b89cfbc264
jackie gan <tuj90887@temple.edu>
2022-04-10 10:10:34 -0400

    somewhat done with chap12

--- MATH3044/homework12_2Q2.m
+++ MATH3044/homework12_2Q2.m
@@ -12,4 +12,47 @@ clc
 Freal = @(x,t)2*exp(-1*(pi^2/4)*t)*sin(2*pi*x);
 ut0 = 0; ut1 = 0;
 ux =@(x) 2*sin(2*pi*x);
-m=3; T=0.1; N=2;
+
+%heat equations Backward-Difference 
+m=10; T=0.1; N=2; l = 1;
+
+%set alpha 
+lalpha = 0.1;
+
+% m=m-1; N=N-1;
+%step 1
+h=l/m; k=T/N; lambda = (lalpha*lalpha*k)/(h*h);
+w = zeros(m,N);
+%step 2 
+for i = 1:m-1
+    w(i,1) = ux(i*h);
+end
+
+l(1) = 1+(2*lambda);
+u(1) = -lambda/l(1);
+
+for i=2:m-2
+    l(i) = 1+(2*lambda)+(lambda*u(i-1));
+    u(i) = -lambda/l(i);
+end
+%step 5
+l(m-1) = 1+(2*lambda)+(lambda*u(m-2));
+%step 6
+for j = 1:N-1
+    t(j) = j*k;
+    z(1) = w(1,j)/l(1);
+    for i = 2:m-1
+        z(i)= (w(i,j)+(lambda*z(i-1)))/l(i);
+    end
+    w(m-1,j+1) = z(m-1);
+    for i = m-2:-1:1
+        w(i,j+1) = z(i)-(u(i)*w(i+1,j+1));
+    end
+    fprintf("t: %0.2f\n",t);
+    for i = 1:m-1
+        x = i*h;
+        fprintf("x:%0.2f,\t w:%0.4f\n",x,w(i,j+1));
+%         fprintf();
+    end
+end
+
--- MATH3044/homework12_2Q5.m
+++ MATH3044/homework12_2Q5.m
@@ -13,6 +13,51 @@ Freal = @(x,t)exp(-4*pi*pi*t)*sin(2*pi*x);
 hreal = 0.4;
 kreal = 0.05;
 
-h = 0.4;
-k = 0.1;
-t= 0.5;
+%Crank-Nicolson Method
+% h = 0.4;
+% k = 0.1;
+% t= 0.5;
+ux = @(x) sin(2*pi*x);
+%input l(endpoint), maxTime T, choose alpha,m>=3, N>=1
+l=2;
+T=2;
+N=1;
+m=3;
+
+alpha = 0.5;
+
+h = l/m; k = T/N; lambda = (alpha*alpha*k)/(h*h);
+%w = zeros(m,N);
+
+for i=1:m-1
+    w(i,1) = ux(i*h);
+end
+
+l(1)= 1+lambda;
+u(1)= -lambda/(2*l(1));
+
+for i= 2:m-2
+    l(i)=1+lambda+(lambda*u(i-1))/2;
+    u(i)=-lambda/(2*l(i));
+end
+
+l(m-1)=(1+lambda+(lambda*u(m-2)))/2;
+for j=1:N
+    t(j)=j*k;
+    z(1)=(((1-lambda)*w(1,))*((lambda/2)*w(1,2)));
+    for i = 2:m-1
+        z(i)=(((1-lambda)*w(i,j))+((lambda/2)*(w(i+1,j)+w(i-1,j)+z(i-1))))/l(i);
+    end
+    w(m-1)=z(m-1);
+    for i = m-2:-1:1
+        w(i,j)=z(i)+u(i)*w(i+1,j);
+    end
+
+    for i=1:m-1
+        x=i*h;
+        fprintf("x:%0.2f,\t w:%0.4f\n",x,w(i,j));
+    end
+end
+
+
+
